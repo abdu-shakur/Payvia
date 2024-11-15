3f9689b23333d85ebf4009f767c4edc4cd43a2e6
Latifat <alateefah.04@gmail.com>
2024-11-15 03:12:34 +0100

    .

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/",
   plugins: [react()],
+    server: {
+      proxy: {
+        '/api': 'http://localhost:8000',
+      },
+    },
+  
 })
