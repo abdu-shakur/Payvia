4f518ab38081d488ef5623f2955951b8d4bc3d24
Latifat <alateefah.04@gmail.com>
2024-11-15 02:32:17 +0100

    .

--- server/server.js
+++ server/server.js
@@ -1,21 +1,30 @@
-const express = require('express')
-const dotenv = require('dotenv').config()
-const connectDB = require('./dbConfig/dbConfig')
-const cors = require('cors')
-
-
-const port = process.env.PORT || 5000;
+const express = require('express');
+const dotenv = require('dotenv').config();
+const connectDB = require('./dbConfig/dbConfig');
+const cors = require('cors');
 
 const app = express();
 
-app.use(cors());
+// CORS Configuration (adjust with your frontend URL)
+const corsOptions = {
+  origin: 'https://payvia.vercel.app', // Update with your actual frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+};
+app.use(cors(corsOptions));
+
+// Middleware for parsing JSON and URL-encoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/dashboard', require('./routes/transactionRoutes'), require('./routes/fundsRoutes'));
-
+// Connect to Database
 connectDB();
-app.listen(port, ()=> 
-    console.log(`Server listening on port ${port}`)
-)
+
+// Define routes for API
+app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/transactions', require('./routes/transactionRoutes'));
+app.use('/api/funds', require('./routes/fundsRoutes'));
+
+// Export the app (for Vercel's serverless function handler)
+module.exports = app;
+
+
