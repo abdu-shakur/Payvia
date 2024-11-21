5263006b8574e8dfa5198884afc6914883920a48
Latifat <alateefah.04@gmail.com>
2024-11-21 10:42:28 +0100

    .

--- client/src/utils/LoggedIn.jsx
+++ client/src/utils/LoggedIn.jsx
@@ -1,7 +1,17 @@
 import { Navigate } from 'react-router-dom';
+import {jwtDecode} from "jwt-decode"
 const LoggedIn = () =>{
     const token = localStorage.getItem('token')
-    return !!token
+    if (!token){
+      return false;
+    }
+    try {
+      const decoded = jwtDecode(token);
+      console.log(decoded)
+      return decoded.exp * 1000 > Date.now();
+    } catch (error) {
+      return false
+    }
   }
 
   const AllowAuthUser = (comp) =>{
