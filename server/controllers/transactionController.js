3ec54f0eedcac1b4369645491d272880ea42b319
Latifat <alateefah.04@gmail.com>
2024-11-22 21:06:31 +0100

    .

--- server/controllers/transactionController.js
+++ server/controllers/transactionController.js
@@ -135,7 +135,7 @@ const generateReferenceCode = (userId) => {
   
   const sendReceipt = {
     From: process.env.DEMO_EMAIL,
-    To: 'velmolimlo@gufum.com',
+    To: 'zmk28862@inohm.com',
     Subject: 'Transaction Reciept',
     HtmlBody: `<h1>YOur Transaction Reciept</h1>
     <p></p>
@@ -240,7 +240,7 @@ const verifyTransaction = asyncHandler(async (req, res) => {
 
       const sendReceipt = {
         From: process.env.DEMO_EMAIL,
-        To: 'velmolimlo@gufum.com',
+        To: 'zmk28862@inohm.com',
         Subject: 'Transaction Reciept',
         HtmlBody: `<h1>YOur Transaction Reciept</h1>
         <p></p>
